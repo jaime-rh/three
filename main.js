d83c9d348cee8cb6bbdc9c64363ea7c102ab727d
jaime-rh <jaimeromerohernandez4@gmail.com>
2026-04-20 16:41:45 +0200

    prueba de el three en iframe + segundo modelo rotando con boton simulando alarma

--- main.js
+++ main.js
@@ -4,6 +4,21 @@ import { FBXLoader } from 'https://cdn.jsdelivr.net/npm/three@0.152/examples/jsm
 import { OBJLoader } from 'https://cdn.jsdelivr.net/npm/three@0.152/examples/jsm/loaders/OBJLoader.js';
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.152/examples/jsm/controls/OrbitControls.js';
 
+//modelos fuera del loader
+let model1;
+let model2;
+
+let model2InitialPosition = null;
+let model2InitialRotation = null;
+
+let valor = 0; // estado inicial
+
+  document.getElementById("alarma").addEventListener("click", () => {
+    valor = valor === 0 ? 1 : 0;
+    console.log("Valor actual de la alarma:", valor);
+  });
+
+
 // Escena
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x222222);
@@ -44,23 +59,60 @@ const loader = new GLTFLoader();
 loader.load(
   './P_CalleReal84_2.glb',
   (gltf) => {
-    const model = gltf.scene;
-    scene.add(model);
 
-    // calcular bounding box
-    const box = new THREE.Box3().setFromObject(model);
-    const center = box.getCenter(new THREE.Vector3());
+    model1 = gltf.scene;
+    scene.add(model1);
 
     // centrar modelo
-    model.position.x -= center.x;
-    model.position.y -= center.y;
-    model.position.z -= center.z;
-  },
-  undefined,
-  (error) => {
-    console.error('Error cargando modelo:', error);
+    const box = new THREE.Box3().setFromObject(model1);
+    const center = box.getCenter(new THREE.Vector3());
+
+    model1.position.x -= center.x;
+    model1.position.y -= center.y;
+    model1.position.z -= center.z;
   }
 );
+
+//Loader2
+const loader2 = new FBXLoader();
+
+loader2.load('./caj.fbx', (fbx) => {
+  model2 = fbx;
+  scene.add(model2);
+
+  model2.scale.set(0.01, 0.01, 0.01); 
+  model2.position.set(11, -1, 1);
+
+  // Guardar posición y rotación inicial
+  model2InitialPosition = model2.position.clone();
+  model2InitialRotation = model2.rotation.clone();
+
+  fbx.traverse((child) => {
+    if (child.isMesh) {
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+});
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  if (model2) {
+    if (valor === 1) {
+      model2.rotation.y += 0.04;
+    } else {
+      // Restaurar posición y rotación
+      model2.position.copy(model2InitialPosition);
+      model2.rotation.copy(model2InitialRotation);
+    }
+  }
+
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+animate();
 
 /*
 
@@ -108,7 +160,7 @@ window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-// Animación
+/* Animación
 function animate() {
   requestAnimationFrame(animate);
 
@@ -118,3 +170,4 @@ function animate() {
 }
 
 animate();
+*/
