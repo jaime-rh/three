7e9984782a5e19251907fbda732f920675dfad19
jaime-rh <jaimeromerohernandez4@gmail.com>
2026-04-20 13:55:58 +0200

    extensiones de modelos (glb, obj, fbx y gltf (con capetas con textutas, etc...))

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.152/examples/jsm/loaders/GLTFLoader.js';
+import { FBXLoader } from 'https://cdn.jsdelivr.net/npm/three@0.152/examples/jsm/loaders/FBXLoader.js';
+import { OBJLoader } from 'https://cdn.jsdelivr.net/npm/three@0.152/examples/jsm/loaders/OBJLoader.js';
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.152/examples/jsm/controls/OrbitControls.js';
 
 // Escena
@@ -36,24 +38,68 @@ const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 directionalLight.position.set(5, 10, 5);
 scene.add(directionalLight);
 
-// Loader GLTF
+//Loader GLB/GLTF
 const loader = new GLTFLoader();
 
 loader.load(
-  './lobo.glb', // asegúrate de que la ruta es correcta
+  './PLANTA/scene.gltf',
   (gltf) => {
     const model = gltf.scene;
     scene.add(model);
 
-    // Ajustes opcionales
-    model.position.set(0, 0, 0);
-    model.scale.set(1, 1, 1);
+    // calcular bounding box
+    const box = new THREE.Box3().setFromObject(model);
+    const center = box.getCenter(new THREE.Vector3());
+
+    // centrar modelo
+    model.position.x -= center.x;
+    model.position.y -= center.y;
+    model.position.z -= center.z;
   },
   undefined,
   (error) => {
     console.error('Error cargando modelo:', error);
   }
 );
+
+/*
+
+// Loader FBX
+const loader = new FBXLoader();
+
+loader.load('./caj.fbx', (fbx) => {
+  scene.add(fbx);
+
+  // Ajustes típicos
+  fbx.scale.set(0.01, 0.01, 0.01); 
+  fbx.position.set(0, 0, 0);
+
+  // Opcional: sombras
+  fbx.traverse((child) => {
+    if (child.isMesh) {
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+});
+
+// Loader OBJ
+const loader = new OBJLoader();
+
+loader.load('./Cajero.obj', (obj) => {
+
+  // Material básico (OBJ no siempre trae materiales bien)
+  obj.traverse((child) => {
+    if (child.isMesh) {
+      child.material = new THREE.MeshStandardMaterial({ color: 0xaaaaaa });
+    }
+  });
+
+  obj.scale.set(1, 1, 1);
+  scene.add(obj);
+});
+
+*/
 
 // Resize
 window.addEventListener('resize', () => {
